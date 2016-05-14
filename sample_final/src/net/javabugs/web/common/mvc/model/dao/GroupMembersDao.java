f04d0fcd40621b08fdc30aa47fcc840f1cd0e204
eminentstar <junk3843@naver.com>
2016-05-15 00:49:48 +0900

    test2

--- sample_final/src/net/javabugs/web/common/mvc/model/dao/GroupMembersDao.java
+++
@@ -1,9 +0,0 @@
-package net.javabugs.web.common.mvc.model.dao;
-
-import net.javabugs.web.common.mvc.model.dto.GroupMember;
-
-public interface GroupMembersDao {
-	public int addUserToGroupMember(GroupMember groupMembers);
-	
-	public int updateUserLevel(GroupMember GroupMember);
-}
--- sample_final/src/net/javabugs/web/common/mvc/model/dao/SecurityManagerDao.java
+++
@@ -1,8 +0,0 @@
-package net.javabugs.web.common.mvc.model.dao;
-
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-public interface SecurityManagerDao extends UserDetailsService {
-	
-	public int changePassword(String users_name, String oldPasswd, String newPasswd);
-}
